1e8c741188451f2d6d4a360f81402a803fe94368
mariratamosi <rongrazzo@gmail.com>
2023-11-11 03:01:55 +0600

    yess modalloadingt

--- app/components/main/doctorsHomePage/doctorsAppointmentModal.js
+++ app/components/main/doctorsHomePage/doctorsAppointmentModal.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
 import FullScreenModal from "../../modals/fullScreenModal";
+import BasicCalendar from "../../calender/basicCalendar";
 
 const DoctorsAppointmentModal = ({
   onHideAppointmentClick,
   doctorId,
   open,
 }) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //get doctors available date
+  //disable past date
+  //after clicking on date populate time
+  //submit the time
+  //during submission - check auth
 
   useEffect(() => {
     console.table({
@@ -22,7 +29,19 @@ const DoctorsAppointmentModal = ({
         isOpen={true}
         onHide={onHideAppointmentClick}
         isLoading={isLoading}
-      />
+      >
+        <div className="flex flex-wrap justify-around h-full">
+          <div className="flex-1 md:w-80   border-gray-300 md:border-r md:h-full pt-5">
+            Docors info
+          </div>
+          <div className="flex-1 md:w-80  border-gray-300 md:border-r h-full pt-5">
+            <BasicCalendar />
+          </div>
+          <div className="flex-1 md:w-80  border-gray-300 h-full pt-5">
+            Time
+          </div>
+        </div>
+      </FullScreenModal>
     )
   );
 };
--- app/components/modals/fullScreenModal.js
+++ app/components/modals/fullScreenModal.js
@@ -1,23 +1,18 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import ListItemText from "@mui/material/ListItemText";
-import ListItem from "@mui/material/ListItem";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import CloseIcon from "@mui/icons-material/Close";
 import Slide from "@mui/material/Slide";
-import Loading from "@/app/Utility/loading";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function FullScreenModal({ isOpen, onHide }) {
+export default function FullScreenModal({ isOpen, onHide, children }) {
   const [open, setOpen] = React.useState(isOpen);
 
   React.useEffect(() => {
@@ -56,19 +51,8 @@ export default function FullScreenModal({ isOpen, onHide }) {
             </Button>
           </Toolbar>
         </AppBar>
-        <List>
-          <ListItem button>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText
-              primary="Default notification ringtone"
-              secondary="Tethys"
-            />
-          </ListItem>
-        </List>
-        <Loading />
+
+        {children}
       </Dialog>
     </React.Fragment>
   );
